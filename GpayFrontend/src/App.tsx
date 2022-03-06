9b04e5aecd0addfb7a993b3868f21e205038169e
Richard Ewin-Lambert <rewinlambert@deloitte.co.uk>
2022-03-06 20:41:39 +0000

    NewEmployee component added

--- GpayFrontend/src/App.tsx
+++ GpayFrontend/src/App.tsx
@@ -2,11 +2,13 @@ import React from 'react';
 import { Routes, Route } from 'react-router';
 import { HashRouter } from 'react-router-dom';
 import Dashboard from './components/Dashboard/Dashboard';
+import NewEmployee from './components/NewEmployee/NewEmployee';
 
 const App = () => (
   <HashRouter>
     <Routes>
       <Route path="/" element={<Dashboard />} />
+      <Route path="/employees" element={<NewEmployee />} />
     </Routes>
   </HashRouter>
 );
