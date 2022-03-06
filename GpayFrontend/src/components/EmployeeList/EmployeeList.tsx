d617c17ffbee6f67a9c91096b26c7556e9aaa6fd
Richard Ewin-Lambert <rewinlambert@deloitte.co.uk>
2022-03-06 22:35:36 +0000

    mapped out Data and created a table

--- GpayFrontend/src/components/EmployeeList/EmployeeList.tsx
+++ GpayFrontend/src/components/EmployeeList/EmployeeList.tsx
@@ -1,5 +1,15 @@
 import React from 'react'
+import { EmployeeData } from '../../constant/types';
 import { useFetchEmployees } from '../../hooks/useApi';
+import {
+  StyledTable,
+  TR,
+  TH,
+  TD,
+  THead,
+  TBody,
+  TableContainer,
+} from './EmployeeList.styles';
 
 const EmployeeList = () => {
   const { employeeData, isError, isLoading } = useFetchEmployees();
@@ -7,7 +17,34 @@ const EmployeeList = () => {
   console.log(employeeData)
 
   return (
-    <div>EmployeeList</div>
+    <>
+      <TableContainer>
+        {employeeData.length > 0 &&
+          <StyledTable>
+            <THead>
+              <TR>
+                <TH>Name</TH>
+                <TH>E-mail</TH>
+                <TH>Phone Number</TH>
+                <TH>Country</TH>
+                <TH>Date Created</TH>
+              </TR>
+            </THead>
+            <TBody>
+              {employeeData.map((employee: EmployeeData) => (
+                <TR key={employee.id}>
+                  <TD>{employee.name}</TD>
+                  <TD>{employee.phoneNumber}</TD>
+                  <TD>{employee.email}</TD>
+                  <TD>{employee.country}</TD>
+                  <TD>{employee.InDtTm}</TD>
+                </TR>
+              ))}
+            </TBody>
+          </StyledTable>
+        }
+      </TableContainer>
+    </>
   )
 }
 
