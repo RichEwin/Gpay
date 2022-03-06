28c85042d16db9b1005330455ba2d1550d61b96a
Richard Ewin-Lambert <rewinlambert@deloitte.co.uk>
2022-03-06 20:53:53 +0000

    Added navigation to /employees

--- GpayFrontend/src/components/Dashboard/Dashboard.tsx
+++ GpayFrontend/src/components/Dashboard/Dashboard.tsx
@@ -1,13 +1,21 @@
 import React from 'react'
 import { ButtonAddEmployee, Header, HeaderContainer } from './Dashboard.styles';
+import { useNavigate } from 'react-router';
 
 const Dashboard = () => {
   const header = 'giggers';
+  const navigate = useNavigate();
+
+  const addEmployeeClickHandler = () => {
+    navigate('/employees');
+  }
+
   return (
     <>
       <HeaderContainer>
         <Header>{header}</Header>
         <ButtonAddEmployee
+          onClick={addEmployeeClickHandler}
         >
           add gigger
         </ButtonAddEmployee>
